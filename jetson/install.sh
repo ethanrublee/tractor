d47e1818e33b7bb8c01bd66517cb448e4d92333d
Ian Sherman <ian.j.sherman@gmail.com>
2020-09-17 08:50:53 -0700

    Resolve merge conflict in install.sh

--- jetson/install.sh
+++ jetson/install.sh
@@ -9,23 +9,16 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 export SERVICE_DIR=$( cd "$( dirname "${SOURCE}" )" >/dev/null 2>&1 && pwd )
 
-<<<<<<< HEAD
-# clean up old systemctl services
-rm -f /etc/systemd/system/multi-user.target.wants/tractor*
-rm -f /etc/systemd/system/tractor*
-
-=======
 # install uhubctl
->>>>>>> upstream/master
 mkdir -p /opt/farm_ng/systemd
 prefix=/opt/farm_ng make -C $SERVICE_DIR/uhubctl
 prefix=/opt/farm_ng make -C $SERVICE_DIR/uhubctl install
 
 # clean
 systemctl list-unit-files | grep "tractor" | awk '{print $1}' | xargs --no-run-if-empty -n1 sudo systemctl disable
+rm -f /etc/systemd/system/multi-user.target.wants/tractor*
 rm -f /opt/farm_ng/systemd/*.sh
-rm -f /etc/systemd/system/tractor*.service
-rm -f /etc/systemd/system/tractor*.path
+rm -f /etc/systemd/system/tractor*
 
 # install
 cp $SERVICE_DIR/*.sh /opt/farm_ng/systemd
@@ -41,20 +34,7 @@ systemctl daemon-reload
 
 # start automatically on boot
 systemctl enable tractor-bringup.service
-<<<<<<< HEAD
 systemctl enable tractor.path
 systemctl enable tractor-steering.path
 systemctl enable tractor-camera.path
 systemctl enable tractor-webservices.path
-=======
-systemctl enable tractor-ready.path
-systemctl enable tractor-steering.service
-systemctl enable tractor.service
-systemctl enable tractor-webservices.service
-
-# start immediately
-systemctl start tractor-ready.path
-systemctl start tractor-steering.service
-systemctl start tractor-bringup.service
-systemctl start tractor-webservices.service
->>>>>>> upstream/master
